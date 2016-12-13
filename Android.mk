783782f3df5403a9292de8eb92367f2f2a1b2934
Rob Clark <robdclark@gmail.com>
2016-12-13 15:55:42 -0500

    tests-3d: add shader-size test

--- Android.mk
+++ Android.mk
@@ -264,6 +264,14 @@ LOCAL_LDLIBS := -llog -lc -ldl -lEGL -lGLESv2
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE    := test-shader-size
+LOCAL_SRC_FILES := tests-3d/test-shader-size.c
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/includes $(LOCAL_PATH)/util
+LOCAL_CFLAGS := -DBIONIC -std=c99
+LOCAL_LDLIBS := -llog -lc -ldl -lEGL -lGLESv3
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE    := test-srgb-fbo
 LOCAL_SRC_FILES := tests-3d/test-srgb-fbo.c
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/includes $(LOCAL_PATH)/util
