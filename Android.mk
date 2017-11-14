772695249406d8384a15d23f64977ffd420a5b61
Rob Clark <robdclark@gmail.com>
2017-11-14 11:32:37 -0500

    test-3d: add test-draw-indirect

--- Android.mk
+++ Android.mk
@@ -120,6 +120,14 @@ LOCAL_LDLIBS := -llog -lc -ldl -lEGL -lGLESv2
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE    := test-draw-indirect
+LOCAL_SRC_FILES := tests-3d/test-draw-indirect.c
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/includes $(LOCAL_PATH)/util
+LOCAL_CFLAGS := -DBIONIC -std=c99
+LOCAL_LDLIBS := -llog -lc -ldl -lEGL -lGLESv3
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE    := test-enable-disable
 LOCAL_SRC_FILES := tests-3d/test-enable-disable.c
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/includes $(LOCAL_PATH)/util
