520e99592a2a770c278de7b10ff0338b2599ecd3
Rob Clark <robdclark@gmail.com>
2017-11-14 10:09:04 -0500

    tests-3d: add test-limits

--- Android.mk
+++ Android.mk
@@ -184,6 +184,14 @@ LOCAL_LDLIBS := -llog -lc -ldl -lEGL -lGLESv3
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE    := test-limits
+LOCAL_SRC_FILES := tests-3d/test-limits.c
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/includes $(LOCAL_PATH)/util
+LOCAL_CFLAGS := -DBIONIC -std=c99
+LOCAL_LDLIBS := -llog -lc -ldl -lEGL -lGLESv3
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE    := test-lrz
 LOCAL_SRC_FILES := tests-3d/test-lrz.c
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/includes $(LOCAL_PATH)/util
